783c3b9b6bcbbdcbe6c24979fd77fd7647463cea
luapvu <luapvu@gmail.com>
2015-11-30 01:00:47 -0800

    MSE function update
    Started MSE function.

--- MSE.m
+++ MSE.m
@@ -1,28 +1,14 @@
-function [i, j, val] = MSE( Yc,Uc,Vc, Yr,Ur,Vr mbSize)
-currentYFrame = Yc{1,1}(1,1);
-currentUFrame = Uc{1,1}(1,1);
-currentVFrame = Vc{1,1}(1,1);
+function [MSE ] = MSE( Y, MB_width, MB_height, frameSize)
+MSE = 0
+for i = 1:MB_width*MB_height
 
-referenceYFrame = Yr{1,2}(1,1);
-referenceUFrame = Ur{1,2}(1,1);
-referenceVFrame = Vr{1,2}(1,1);
+    for j = 1:MB_width*MB_height
+    MSE = MSE + (abs(Y{1:i}(1,j) - Y{1:i}(1 , j+1)))
+    end
+end
 
-MBSIZE = mbSize;
-sum = 0;
-k = 1;
- for i = 1:144
-    
-     for j = 1:176
-       val = Y{1,i}(k,j)
-     i = i
-     j = j
-     end
-      k = k+1
-    
- end
- 
+
 
 
 end
-
 
